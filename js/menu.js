2ea91df4de7101aac2b4f0854a2f1aa95358602c
xjtx <73387366+xiaoji235@users.noreply.github.com>
2021-12-11 10:58:31 +0800

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,42 @@
+const searchEngines = [{
+    "id": 0,
+    "name": "百度",
+    "img": "url('search\/baidu.svg')",
+    "url": "https:\/\/www.baidu.com\/s?wd="
+},
+{
+    "id": 1,
+    "name": "搜狗",
+    "img": "url('search\/sogou.svg')",
+    "url": "https:\/\/www.sogou.com\/web?query="
+},
+{
+    "id": 2,
+    "name": "360",
+    "img": "url('search\/360.svg')",
+    "url": "https:\/\/www.so.com\/s?q="
+},
+{
+    "id": 3,
+    "name": "Google",
+    "img": "url('search\/google.svg')",
+    "url": "https:\/\/www.google.com\/search?q="
+},
+{
+    "id": 4,
+    "name": "Bing",
+    "img": "url('search\/bing.svg')",
+    "url": "https:\/\/cn.bing.com\/search?q="
+},
+{
+    "id": 19,
+    "name": "Github",
+    "img": "url('search\/github.svg')",
+    "url": "https:\/\/github.com\/search?q="
+},
+{
+    "id": 7,
+    "name": "Yandex",
+    "img": "url('search\/yandex.svg')",
+    "url": "https:\/\/yandex.com\/search\/?text="
+}];
